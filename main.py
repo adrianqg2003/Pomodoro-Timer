8a74963c9c3f1ff4567ce99165e0da6074343667
adrianqg2003 <adrianquesada003@gmail.com>
2025-07-24 18:34:11 +0200

    imports

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from pomodoro.ui import PomodoroWindow
+
+def main():
+    app = QApplication(sys.argv)
