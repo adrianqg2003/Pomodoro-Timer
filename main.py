66a817fb35ec143ca678996a07c1877ae494ee32
adrianqg2003 <adrianquesada003@gmail.com>
2025-07-24 18:31:12 +0200

    imports

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+import sys
+import os
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from pomodoro.ui import PomodoroWindow
